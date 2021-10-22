6683218314010b424ad2a64be853aad803fe0d23
AlexyDaCoder <81732383+alexyy802@users.noreply.github.com>
2021-10-22 12:50:58 +0700

    Update automod.py

--- src/automod.py
+++ src/automod.py
@@ -6,12 +6,22 @@ client = commands.Bot(command_prefix="!")
 
 client = discord.Client()
 
+def read_json(filename):
+    with open(filename, "r") as f:
+        data = json.load(f)
+    return data
+
+
+def write_json(data, filename):
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
+
 
 @client.event
 async def on_message(self,message):
       server = message.guild
       data = read_json("automod.json")
-      automod = data["automod"]
+      automod = data["automodservers"]
       if server.id in automod:
         banword=['Put bad words here']
 
@@ -20,3 +30,11 @@ async def on_message(self,message):
           await message.channel.send(message.author.mention+", Hey! that word isnt allowed in here!")
         else:
           return
+
+@client.command()
+async def hello(ctx,member:discord.Member):
+      await ctx.send('Hello,'+ ctx.author)
+      return
+
+
+client.run(discord-bot-token)
